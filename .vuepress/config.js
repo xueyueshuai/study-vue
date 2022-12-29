f37d3ca1e19101d6f3436b4c75c054c4839d1294
xys <xueyueshuai@xinzhidi.com>
2022-12-29 18:20:23 +0800

    a

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,14 +8,13 @@ module.exports = {
         ],
         sidebar: [
             {
-                title: '笔记',
+                title: 'vue笔记',
                 collapsable: false,
                 children: [
                     ['/guide/createProject/', '创建一个项目'],
                     ['/guide/vueConfig/', '配置vue.config.js'],
                     ['/guide/router/', '路由相关'],
                     ['/guide/request/', '网络请求'],
-
                 ]
             },
             {
